5a6d0508060904bb8ce49521524579d9a5b7cf5e
Subashis120 <69248723+Subashis120@users.noreply.github.com>
2021-11-16 20:58:02 +0530

    Update urls.py

--- movie_review/movie_api/urls.py
+++ movie_review/movie_api/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('movie-list/', views.MoviesList, name='ShowMovies'),
     path('users-list/', views.UsersList, name='ShowUsers'),
     path('user-create/', views.CreateUser, name='CreateUser'),
-    path('category/', views.Category, name='Category'),
-    path('review/', views.Review, name='Review'),
-    path('rating/', views.Rating, name='Review'),
+    path('category/', views.Category1, name='Category'),
+    path('review/', views.Review1, name='Review'),
+    path('review-list/', views.ReviewShow, name='ReviewList'),
+    path('rating-insert/', views.Rating1, name='Rating-Insert'),
+    path('rating/', views.Ratingshow, name='Rating'),
 
 ]
