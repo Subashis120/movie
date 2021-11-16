1994fe07c86ee4ee6ec65207f6ed7b77d6fbb1dc
Subashis120 <69248723+Subashis120@users.noreply.github.com>
2021-11-16 20:58:39 +0530

    Update views.py

--- movie_review/movie_api/views.py
+++ movie_review/movie_api/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .serializers import ApiSerializer
+from .serializers import ApiSerializer, ApiSerializer1, ApiSerializer2, ApiSerializer3, ApiSerializer4, ApiSerializer5
 from .models import Movie, Users, Rating, Review, Category
-from django.contrib.auth.hashers import make_password
+
 
 # Create your views here.
 
@@ -23,58 +23,72 @@ def ApiOverview(request):
         'M-Delete': '/movie-delete/',
         'C-List': '/category/',
         'R-List': '/review/',
+        'Rat-insert': '/rating-insert/',
         'Rat-List': '/rating/',
+        'Review-show': '/review-list/',
 
     }
     return Response(api_urls)
 
 
-@api_view(['Get'])
+@api_view(['GET'])
 def MoviesList(request):
     movies = Movie.objects.all()
     serializer = ApiSerializer(movies, many=True)
     return Response(serializer.data)
 
 
-@api_view(['Get'])
+@api_view(['GET'])
 def UsersList(request):
     users = Users.objects.all()
-    serializer = ApiSerializer(users, many=True)
+    serializer = ApiSerializer5(users, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def Ratingshow(request):
+    rating = Rating.objects.all()
+    serializer1 = ApiSerializer4(rating, many=True)
+    return Response(serializer1.data)
+
+
+@api_view(['GET'])
+def Category1(request):
+    cat = Category.objects.all()
+    serializer3 = ApiSerializer2(cat, many=True)
+    return Response(serializer3.data)
+
+
+@api_view(['GET'])
+def ReviewShow(request):
+    review = Review.objects.all()
+    serializer2 = ApiSerializer3(review, many=True)
+    return Response(serializer2.data)
 
 
 @api_view(['Post'])
 def CreateUser(request):
-    serializer4 = ApiSerializer(data=request.data)
+    serializer4 = ApiSerializer1(data=request.data)
     if serializer4.is_valid():
         serializer4.save()
-
     return Response(serializer4.data)
 
 
 @api_view(['Post'])
-def Rating(request):
-    serializer1 = ApiSerializer(data=request.data)
-
-    if serializer1.is_valid():
-        serializer1.save()
-
-    return Response(serializer1.data)
-
-
-@api_view(['Post'])
-def Review(request):
-    serializer2 = ApiSerializer(data=request.data)
-
+def Review1(request):
+    serializer2 = ApiSerializer3(data=request.data)
     if serializer2.is_valid():
         serializer2.save()
-
     return Response(serializer2.data)
 
 
-@api_view(['Get'])
-def Category(request):
-    category = Category.objects.all()
-    serializer3 = ApiSerializer(category, many=True)
-    return Response(serializer3.data)
+@api_view(['Post'])
+def Rating1(request):
+    serializer1 = ApiSerializer4(data=request.data)
+    if serializer1.is_valid():
+        serializer1.save()
+    return Response(serializer1.data)
+
+
+
 
